2bbf3f2f4731058128c899df34db28c1e8f341ae
internship2025 <incubator.int@gmail.com>
2025-01-30 23:42:56 +0300

    UC-0: init: start formation of the project structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
-export default function Home() {
-  return <h1>Welcome to Next.js</h1>;
+import { Flex, Text, Button } from "@radix-ui/themes";
+
+export default function HomePage() {
+  return (
+      <Flex direction="column" gap="3" p="4">
+        <Text size="5" weight="bold">
+          Welcome to Radix UI with Next.js
+        </Text>
+        <Button variant="classic">Click me</Button>
+      </Flex>
+  );
 }
