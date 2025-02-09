baded85a64352e5babb8e68b4a490da6f073ba15
AlexandrLatushko <latushkoalex94@gmail.com>
2025-02-09 16:14:18 +0300

    add radio and tabs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
 import { Flex, Text } from "@radix-ui/themes";
 import { Button } from "@/common/ui/button/button";
 import { Input } from "@/common/ui/input/input";
+import { RadioButton } from "@/common/ui/radiobutton/radioButton";
+import TabsComponent from "@/common/ui/tabs/tabs";
 
 export default function HomePage() {
+  const tabData = [
+    { value: "tab1", trigger: "Вкладка 1", content: "Контент для вкладки 1" },
+    { value: "tab2", trigger: "Вкладка 2", content: "Контент для вкладки 2" },
+    { value: "tab3", trigger: "Вкладка 3", content: "Контент для вкладки 3" },
+  ];
+
   return (
     <Flex direction="column" gap="3" p="4">
       <Text size="5" weight="bold">
@@ -15,6 +23,11 @@ export default function HomePage() {
       <Input />
       <Input />
       <Input />
+      <RadioButton options={[
+                             {value: '1', label: 'RadioButton' },
+                             { value: '2', label: 'RadioButton'}
+                            ]} />
+      <TabsComponent tabs={tabData} variant={'primary'} />
     </Flex>
   );
 }
