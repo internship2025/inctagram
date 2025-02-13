dea4813601f474efe7764496f3d234965afa5b9c
Veta-s <112316829+Veta-s@users.noreply.github.com>
2025-02-13 19:25:21 +0300

    UC-05 add check-box

---
+++ src/common/ui/checkBox/checkBox.stories.tsx
@@ -0,0 +1,48 @@
+import { CheckBox } from "./checkBox";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof CheckBox> = {
+  component: CheckBox,
+  tags: ["autodocs"],
+  argTypes: {
+    variant: {
+      control: "select",
+      options: ["default", "disable"],
+    },
+    txt:{
+      control:"text"
+    }
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<typeof CheckBox>;
+
+export const WithText: Story = {
+  args: {
+    variant: 'default',
+    txt: 'Check me'
+  },
+};
+
+export const WithoutText: Story = {
+  args: {
+    variant: 'default',
+  },
+};
+
+export const DisableWithText: Story = {
+  args: {
+    variant: 'disable',
+    txt: 'Check me',
+    disabled: true,
+  },
+};
+
+export const DisableWithoutText: Story = {
+  args: {
+    variant: 'disable',
+    disabled: true,
+  },
+};
