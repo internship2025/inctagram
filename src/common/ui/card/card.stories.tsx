ba5f2d45e813d99ac704f931bd1e663db8370300
Vikastgn <vikamarygina@yandex.ru>
2025-02-19 19:40:39 +0300

    UC-05 add card

---
+++ src/common/ui/card/card.stories.tsx
@@ -0,0 +1,15 @@
+import { Card } from "@/common/ui/card/card";
+import { Meta, StoryObj } from "@storybook/react";
+
+const meta: Meta<typeof Card> = {
+  title: "Common/ui/Card",
+  component: Card,
+  tags: ["autodocs"],
+  argTypes: {},
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Card>;
+
+export const Default: Story = {};
