865f7b3de872d7863ed9f467cb0fe8f268d32828
Halanau327 <a.halanau@gmail.com>
2025-02-19 18:35:27 +0300

    UC-1: feat: added sing-up component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,18 @@
 "use client";
 import { Header } from "@/shared/ui/header/header";
+import { ModalRadix } from "@/features/auth/ui/sign-up/sign-up-page";
+import { useState } from "react";
 
 export default function HomePage() {
-  return <Header showAuth={false} />;
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  const onSignUpHandler = () => {
+    setIsSignUp(true);
+  };
+  return (
+    <>
+      <Header showAuth={false} onSignUpClick={onSignUpHandler} />;
+      {isSignUp && <ModalRadix />}
+    </>
+  );
 }
