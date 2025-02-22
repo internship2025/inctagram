9164b6c68de0df71d4a9c4424997f49554589b8b
Halanau327 <a.halanau@gmail.com>
2025-02-22 14:39:51 +0300

    UC-1: fix: change credentials in singup endpoint

--- src/features/auth/ui/signUpModal/SignUpModal.stories.tsx
+++ src/features/auth/ui/signUpModal/SignUpModal.stories.tsx
@@ -2,9 +2,18 @@ import { Meta, StoryObj } from "@storybook/react";
 import { useState } from "react";
 import { Button } from "@/shared/ui/button/button";
 import { SignUpModal } from "./SignUpModal";
+import { store } from "next/dist/build/output/store";
+import { Provider } from "react-redux";
 
 const meta: Meta<typeof SignUpModal> = {
   component: SignUpModal,
+  decorators: [
+    (Story) => (
+      <Provider store={store}>
+        <Story />
+      </Provider>
+    ),
+  ],
 };
 
 export default meta;
