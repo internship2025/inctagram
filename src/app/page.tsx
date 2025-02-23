0db75e7f8069906925b36f0df135783e2e2af4ad
Halanau327 <a.halanau@gmail.com>
2025-02-23 20:26:53 +0300

    UC-1: feat: added routing for singUp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,16 @@
 import { Header } from "@/shared/ui/header/header";
 import { useState } from "react";
 import { SignUpModal } from "@/features/auth/ui/signUpModal/SignUpModal";
-import { StoreProvider } from "@/services/store-provider";
 
 export default function HomePage() {
   const [isSignUp, setIsSignUp] = useState(false);
 
   return (
     <>
-      <StoreProvider>
-        <Header showAuth={false} onSignUpClick={() => setIsSignUp(true)} />
-        {isSignUp && (
-          <SignUpModal open={true} onClose={() => setIsSignUp(false)} />
-        )}
-      </StoreProvider>
+      <Header showAuth={false} onSignUpClick={() => setIsSignUp(true)} />
+      {isSignUp && (
+        <SignUpModal open={true} onClose={() => setIsSignUp(false)} />
+      )}
     </>
   );
 }
