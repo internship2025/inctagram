8f5bf1e464f852b85cd548debd66394af708e8a6
Vikastgn <vikamarygina@yandex.ru>
2025-02-24 14:40:38 +0300

    UC-05 add changes in pagination

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import { Header } from "@/shared/ui/header/header";
 import { useState } from "react";
 import { SignUpModal } from "@/features/auth/ui/signUpModal/SignUpModal";
 import { StoreProvider } from "@/services/store-provider";
+import { Pagination } from "@/shared/ui/pagination/pagination";
 
 export default function HomePage() {
   const [isSignUp, setIsSignUp] = useState(false);
@@ -14,6 +15,7 @@ export default function HomePage() {
         {isSignUp && (
           <SignUpModal open={true} onClose={() => setIsSignUp(false)} />
         )}
+        <Pagination />
       </StoreProvider>
     </>
   );
