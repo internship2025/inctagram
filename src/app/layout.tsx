614aa6dbe428eaa5a0aa4fbd005311ef22f9fbf1
Vadim Ichetovkin <ichetowwwkin@mail.ru>
2025-03-29 23:49:34 +0300

    UC-05: added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
+import { Metadata } from "next";
 import "./globals.css";
 
 import AppProvider from "@/services/appProvider";
+
+export const metadata: Metadata = {
+  title: 'Instagram',
+  description: 'social network'
+}
 
 export default function RootLayout({
   children,
