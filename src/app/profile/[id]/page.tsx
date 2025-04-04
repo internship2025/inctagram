061e11c52c26f644f4d8c84baca61a0bd7791f84
Vadim Ichetovkin <ichetowwwkin@mail.ru>
2025-04-04 04:34:12 +0300

    UC-05

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -2,10 +2,10 @@ import UserProfile from "@/app/components/userProfile/userProfile";
 import { getPostsUser, getPostUser, getPublicUser } from "@/services/userApi";
 
 
-const Profile = async ({ params, searchParams }: { params: { id: string }, searchParams: { postId: string } }) => {
+const Profile = async ({ params, searchParams }: { params: { id: string },   searchParams: { [key: string]: string | string[] | undefined }; }) => {
   const data = await getPublicUser(Number(params.id));
   const posts = await getPostsUser(Number(params.id));
-  const post =searchParams.postId ? await getPostUser(searchParams.postId) : null
+  const post = searchParams.postId ? await getPostUser(searchParams.postId) : null
 
   return (
     <>
