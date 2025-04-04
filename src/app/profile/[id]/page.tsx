74b90fefe2efe107a8130fad557a841352d75dbd
Vadim Ichetovkin <ichetowwwkin@mail.ru>
2025-04-04 04:37:56 +0300

    UC-05: removed warnings for params

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -2,7 +2,11 @@ import UserProfile from "@/app/components/userProfile/userProfile";
 import { getPostsUser, getPostUser, getPublicUser } from "@/services/userApi";
 
 
-const Profile = async ({ params, searchParams }: { params: { id: string },   searchParams: { [key: string]: string | string[] | undefined }; }) => {
+const Profile = async (
+  props: { params: Promise<{ id: string }>,   searchParams: Promise<{ [key: string]: string | string[] | undefined }>; }
+) => {
+  const searchParams = await props.searchParams;
+  const params = await props.params;
   const data = await getPublicUser(Number(params.id));
   const posts = await getPostsUser(Number(params.id));
   const post = searchParams.postId ? await getPostUser(searchParams.postId) : null
